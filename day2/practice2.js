db63a8f7928342d7681419a016694cecd344c1fd
Yan Zhang <149005773+YANZHANG0201@users.noreply.github.com>
2025-03-16 09:03:42 +0000

    chapter2:function

---
+++ day2/practice2.js
@@ -0,0 +1,26 @@
+//函数
+function min(a, b) {
+  return a > b ? b : a;
+}
+
+function pow(x, n) {
+  if (x == 0) return 1;
+  if (x < 0) return 1 / pow(x, -n);
+  let res = 1;
+  while (n > 0) {
+    res = res * x;
+    n = n - 1;
+  }
+  return res;
+}
+//箭头函数
+function ask(question, yes, no) {
+  if (confirm(question)) yes();
+  else no();
+}
+
+ask(
+  "Do you agree",
+  () => alert("you agree"),
+  () => alert("you canceled")
+);
