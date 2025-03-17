af5c6c094bea93d06e27a25b42e759b28528dbd9
Yan Zhang <149005773+YANZHANG0201@users.noreply.github.com>
2025-03-17 08:17:18 +0000

    Chapter3：Object

---
+++ day3/practice1.js
@@ -0,0 +1,34 @@
+//对象方法
+// let calculator = {
+//   read() {
+//     let num1 = +prompt("enter the first one number");
+//     let num2 = +prompt("enter the second number");
+//     this.a = num1;
+//     this.b = num2;
+//   },
+//   sum() {
+//     return this.a + this.b;
+//   },
+//   mul() {
+//     return this.a * this.b;
+//   },
+// };
+
+//构造器和操作符“new”
+// function Calculator() {
+//   this.read = function () {
+//     this.a = +prompt("enter the first number");
+//     this.b = +prompt("enter the second number");
+//   };
+//   this.sum = function () {
+//     return this.a + this.b;
+//   };
+// }
+// function Accumulator(startValue) {
+//   this.value = startValue;
+//   this.read = function () {
+//     this.value += +prompt("enter the number");
+//   };
+// }
+
+//可选链
