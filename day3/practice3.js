a14da8a580d5d1e600e965aab4acfa50605d9b5d
Yan Zhang <149005773+YANZHANG0201@users.noreply.github.com>
2025-03-17 16:01:11 +0000

    chapter3:String

--- day3/practice3.js
+++ day3/practice3.js
@@ -1,1 +1,30 @@
 //字符串
+function ucFirst(str) {
+  let firstChat = str[0].toUpperCase();
+  let newStr = `${firstChat}${str.slice(1)}`;
+  return newStr;
+}
+
+// console.log(ucFirst("john") == "John");
+
+//或者用includes
+function checkSpam(str) {
+  str = str.toLowerCase();
+  if (str.indexOf("viagra") !== -1 || str.indexOf("xxx") !== -1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// console.log(checkSpam("buy ViAgRA now"));
+// console.log(checkSpam("free xxxxx"));
+// console.log(checkSpam("innocent rabbit"));
+
+function truncate(str, maxlength) {
+  if (str.length > maxlength) {
+    //省略号实际上有一个单独的 Unicode 字符，而不是三个点。-->这里应该改成-1
+    str = `${str.slice(0, maxlength - 3)}...`;
+  }
+  return str;
+}
